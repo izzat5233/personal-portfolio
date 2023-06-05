2c8ca2be65f8ed535e402e0f1d91a32441abae66
Izzat Alsharif <izzat5233@gmail.com>
2023-06-05 17:33:57 +0300

    Refactored footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,25 +1,34 @@
 import {LuGithub, LuLinkedin, LuTwitter} from "react-icons/lu";
 
+export function Icon(href, {children}) {
+    return (
+        <a className="transition-all hover:text-accent"
+           href={href} target="_blank" rel="noopener noreferrer">
+            {children}
+        </a>
+    );
+}
+
+export function Signature({children}) {
+    return (
+        <p className="text-3xl font-cursive font-bold">
+            {children}
+        </p>
+    );
+}
+
 export default function Footer() {
     return (
-        <footer className="bg-neutral text-dark font-cursive p-8 mt-4">
+        <footer className="bg-neutral text-dark p-8 mt-4">
             <div className="container mx-auto flex flex-col gap-8 justify-center items-center">
                 <div className="flex gap-5 space-x-4">
-                    <a className="transition-all hover:text-accent"
-                       href="#" target="_blank" rel="noopener noreferrer">
-                        <LuGithub/>
-                    </a>
-                    <a className="transition-all hover:text-accent"
-                       href="#" target="_blank" rel="noopener noreferrer">
-                        <LuLinkedin/>
-                    </a>
-                    <a className="transition-all hover:text-accent"
-                       href="#" target="_blank" rel="noopener noreferrer">
-                        <LuTwitter/>
-                    </a>
+                    <Icon href="#"><LuGithub/></Icon>
+                    <Icon href="#"><LuLinkedin/></Icon>
+                    <Icon href="#"><LuTwitter/></Icon>
                 </div>
-                <p className="text-3xl font-bold">Crafted with <span className="text-accent">
-                    &#10084;</span> by Izzat Alsharif</p>
+                <Signature>
+                    Crafted with <span className="text-accent">&#10084;</span> by Izzat Alsharif
+                </Signature>
             </div>
         </footer>
     );
