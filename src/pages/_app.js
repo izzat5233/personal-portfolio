6599191cebec981af24b5778fc21935ec0ce778b
izzat5233 <izzat5233@gmail.com>
2023-06-23 19:11:49 +0300

    Fix

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -11,8 +11,17 @@ export default function MyApp({Component, pageProps}) {
 
 export function CursorCircleEffect({children}) {
     const [cursorPos, setCursorPos] = useState({x: 0, y: 0});
+    const [isTouchDevice, setIsTouchDevice] = useState(false);
 
     useEffect(() => {
+        if (window.matchMedia("(hover: none)").matches) {
+            setIsTouchDevice(true);
+        }
+    }, []);
+
+    useEffect(() => {
+        if (isTouchDevice) return; // Don't run the effect if it's a touch device
+
         let animationFrameId = null;
         const updateCursorPos = (e) => {
             if (animationFrameId) {
@@ -32,22 +41,25 @@ export function CursorCircleEffect({children}) {
                 window.cancelAnimationFrame(animationFrameId);
             }
         };
-    }, []);
+    }, [isTouchDevice]); // Re-run the effect when isTouchDevice changes
 
     return (
         <div>
-            <div
-                className="fixed transform -translate-x-1/2 -translate-y-1/2
-                 blur-3xl opacity-10 rounded-full pointer-events-none from-fore to-neutral"
-                style={{
-                    backgroundImage: 'radial-gradient(var(--tw-gradient-stops))',
-                    top: cursorPos.y,
-                    left: cursorPos.x,
-                    width: '1000px',
-                    height: '1000px',
-                }}
-            />
+            {!isTouchDevice && (
+                <div
+                    className="fixed transform -translate-x-1/2 -translate-y-1/2
+                     blur-3xl opacity-10 rounded-full pointer-events-none from-fore to-neutral"
+                    style={{
+                        backgroundImage: 'radial-gradient(var(--tw-gradient-stops))',
+                        top: cursorPos.y,
+                        left: cursorPos.x,
+                        width: '1000px',
+                        height: '1000px',
+                    }}
+                />
+            )}
             {children}
         </div>
     );
 }
+
